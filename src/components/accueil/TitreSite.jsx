e5a2520f55d07ff99a4f7d035813819fe5cb25c7
François Chatelier <francois.chatelier@gmail.com>
2021-11-24 19:22:25 +0100

    Début du css pour le titre du site

--- src/components/accueil/TitreSite.jsx
+++ src/components/accueil/TitreSite.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './titreSite.css';
+import avatar from '../images/avatar.png';
 
 const TitreSite = () => {
   return (
     <div className="titre">
       <h1 className="firstname">François</h1>
+      <img src={avatar} alt="avatar" />
       <h2 className="lastname">Chatelier</h2>
     </div>
   );
