0db05e06de92e54319aa42d60a637114e4e50c8f
François Chatelier <francois.chatelier@gmail.com>
2021-11-28 20:18:05 +0100

    Taille image + mise en place

--- src/components/accueil/AccueilPage.jsx
+++ src/components/accueil/AccueilPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './accueilPage.css';
 import NavBar from '../navbar/NavBar';
+import mer from '../images/mer.jpg';
 
 const PageAccueil = () => {
   return (
     <div>
       <NavBar />
+      <div className="photo">
+        <img className="mer" src={mer} alt="Moi même à la mer" />
+      </div>
     </div>
   );
 };
