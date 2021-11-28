742243dbd5e36f953fade4c8d742cf3fd914d7b3
François Chatelier <francois.chatelier@gmail.com>
2021-11-28 22:21:55 +0100

    texte de présentation

--- src/components/accueil/AccueilPage.jsx
+++ src/components/accueil/AccueilPage.jsx
@@ -8,6 +8,14 @@ const PageAccueil = () => {
     <div>
       <NavBar />
       <div className="photo">
+        <div className="grid-intro">
+          <h1 className="intro">Bienvenue !</h1>
+          <h3 className="presentation">
+            Je suis <strong className="name">François Chatelier</strong>,
+            Développeur web Junior.
+          </h3>
+          <p>ReactJs & Express</p>
+        </div>
         <img className="mer" src={mer} alt="Moi même à la mer" />
       </div>
     </div>
