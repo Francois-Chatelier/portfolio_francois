b53c96a3c9ca026dbf40edaa81c5812a8bdb4163
François Chatelier <francois.chatelier@gmail.com>
2021-12-04 19:59:42 +0100

    Mise en place photo page Apropos

--- src/components/apropos/Apropos.jsx
+++ src/components/apropos/Apropos.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './apropos.css';
+import portrait from '../images/Francois_Chatelier.jpg';
 
 const Apropos = () => {
   return (
-    <div>
-      <div>a propos</div>
+    <div className="positionPhoto">
+      <img
+        src={portrait}
+        alt="Portrait François Chatelier"
+        className="portrait"
+      />
+      <div className="spitch">
+        <div className="containerAbout">
+          <div className="card">
+            <h1 className="entete">Un peu de moi</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
